97d3a9fbfb06b9436ff1c46c631bd22b591c6dcb
Gbiterium <58435482+Gbiterium@users.noreply.github.com>
2024-05-24 15:03:45 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ export const fetchTenders = () => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve([
-          { id: 1, title: 'New Tender', description: 'Description 1', deadline: '2024-12-31', document: '/docs/tender1.pdf' },
+          { id: 1, title: 'New Tender', description: 'Description 1', deadline: '2024-12-31', document: 'https://beige-orelle-33.tiiny.site/' },
           { id: 2, title: 'Another Tender', description: 'Description 2', deadline: '2024-11-30', document: '/docs/tender2.pdf' },
           { id: 3, title: 'Tender 3', description: 'Description 3', deadline: '2024-08-30', document: '/docs/tender3.pdf' },
           { id: 4, title: 'Tender 4', description: 'Description 4', deadline: '2024-05-30', document: '/docs/tender4.pdf' },
